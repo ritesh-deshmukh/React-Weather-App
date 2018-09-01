f96c379d3303c7f771a36bd6e3abda82196b4956
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-09-01 18:48:42 -0500

    project skeleton

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Titles from "./components/Titles.js"
+import Form from "./components/Form.js"
+import Weather from "./components/Weather.js"
+
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <Titles />
+          <Form />
+          <Weather />
+        </div>
+      );
+  }
+};
+
+export default App;
