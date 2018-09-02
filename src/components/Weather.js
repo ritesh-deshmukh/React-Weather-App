1e4c248e51bb710a6bab0305e69d21bbc527decc
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-09-02 15:00:00 -0500

    api result display

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,35 @@ import React from 'react';
 class Weather extends React.Component {
 	render(){
 		return(
-				<div>Weather component</div>
+				<div>
+					{
+						this.props.city && 
+						this.props.country && 
+							<p>Location: {this.props.city}, 
+										 {this.props.country}
+							</p>
+					}					
+					{ 
+						this.props.temperature && 
+							<p>temperature: {this.props.temperature}
+							</p>
+					}
+					{ 
+						this.props.humidity &&
+							<p>Humidity: {this.props.humidity}
+							</p>
+					}
+					{
+						this.props.description && 
+							<p>Conditions: {this.props.description}
+							</p>
+					}
+					{
+						this.props.error &&
+							<p>{this.props.error}
+							</p>
+					}
+				</div>
 			);
 	}
 }
